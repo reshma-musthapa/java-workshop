b9b724fecb503634e30db5b36538fd1ea2537919
reshma-musthapa <reshmamusthapa5@gmail.com>
2025-07-10 09:20:14 +0530

    greetings

---
+++ src/com/java/workshop/basics/greetings.java
@@ -0,0 +1,10 @@
+package com.java.workshop.basics;
+
+public class greetings {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("Let's learn and grow together!");
+    }
+}
