9be7a598f3ba7a409142b377b927adf5b8e6af83
reshma-musthapa <reshmamusthapa5@gmail.com>
2025-07-10 15:53:52 +0530

    studentmap

---
+++ src/main/java/com/java/workshop/collection/StudentMap.java
@@ -0,0 +1,21 @@
+package com.java.workshop.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentMap {
+    public static void main(String[] args) {
+        Map<Integer, String> students = new HashMap<>();
+        
+            // Adding students to the map
+           students.put(101, "Caleb");
+            students.put(102, "Shahul");
+            students.put(103, "Nowfiya");
+            students.put(104, "Reshma");
+            students.put(105, "Nitha");
+            students.put(106, "Nowrin");
+            
+        
+    }
+
+}
